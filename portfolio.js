ec74ed820410cf98a6d4c0851e1ee2626627951c
Ludmi Lopez <ludmit@outlook.com>
2022-08-17 14:46:37 -0300

    Testing-responsive

--- portfolio.js
+++ portfolio.js
@@ -17,7 +17,7 @@ var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 3,
+  slidesPerView: 'auto',
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
