e186ee6f495010a906b7181f3245d2c7933c3260
Ludmi Lopez <ludmit@outlook.com>
2022-08-19 16:20:58 -0300

    projects-update

--- portfolio.js
+++ portfolio.js
@@ -13,24 +13,9 @@ const copyMessage2 = document.querySelector(".copyMessage2")
 
 // Swiper config
 
-var swiper = new Swiper(".team-slider", {
-  loop:true,
-  grabCursor:true,
-  spaceBetween:20,
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-      0: {
-          slidesPerView: 1,
-      },
-      768: {
-          slidesPerView: 2,
-      },
-      991: {
-          slidesPerView: 3,
-      },
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
   },
 });
 
